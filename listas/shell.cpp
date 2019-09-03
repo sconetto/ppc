3ff4815d91e719640bd47b8384964756774c3387
João Pedro Sconetto <sconetto.joao@gmail.com>
2019-09-03 16:25:50 -0300

    Adding new class and some files
    Adds newest class about data structures
    
    Adds shell and other files
    
    Signed-off-by: João Pedro Sconetto <sconetto.joao@gmail.com>

---
+++ listas/shell.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}
